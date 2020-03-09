1bd51d5d8326da87653f537276af61632e1cdc30
hyperbolicj <60232039+hyperbolicj@users.noreply.github.com>
2020-03-08 20:42:18 -0500

    updates to about & projects

--- scripts/script.js
+++ scripts/script.js
@@ -181,6 +181,166 @@ app.controller(
                     }
                 ]
             },
+            {
+                title: 'Technical Experience',
+                place: [
+                    {
+                        name: 'Programming Languages',
+                        description: [
+                            {
+                                acomplishment: 'C++, Java, C#, JavaScript, C, Python, Haskel, Perl',
+                                dateStart: new Date('', '', ''),
+                                dateEnd: new Date('', '', '')
+                            }
+                        ]
+                    },
+                    {
+                        name: 'Design Patterns',
+                        description: [
+                            {
+                                acomplishment: 'Bridge, Builder, Chain of responsibility, Command, Decorator, Factory, Flyweight, Mediator, Memento, Object pool, Observer, Proxy, Singleton, State, Strategy, Visitor, MVC, Iterator',
+                                dateStart: new Date('', '', ''),
+                                dateEnd: new Date('', '', '')
+                            }
+                        ]
+                    },
+                    {
+                        name: 'Web Development Skills',
+                        description: [
+                            {
+                                acomplishment: 'AngularJS, ASP.NET, CSS, Html',
+                                dateStart: new Date('', '', ''),
+                                dateEnd: new Date('', '', '')
+                            }
+                        ]
+                    },
+                    {
+                        name: 'High Performance Programming Skills',
+                        description: [
+                            {
+                                acomplishment: 'SIMD Intel Intrinsics, CUDA, OpenMP',
+                                dateStart: new Date('', '', ''),
+                                dateEnd: new Date('', '', '')
+                            }
+                        ]
+                    },
+                    {
+                        name: 'Database Skills',
+                        description: [
+                            {
+                                acomplishment: 'MongoDB, SQL',
+                                dateStart: new Date('', '', ''),
+                                dateEnd: new Date('', '', '')
+                            }
+                        ]
+                    },
+                    {
+                        name: 'Software Testing Skills',
+                        description: [
+                            {
+                                acomplishment: 'JUnit, MSTest',
+                                dateStart: new Date('', '', ''),
+                                dateEnd: new Date('', '', '')
+                            }
+                        ]
+                    },
+                    {
+                        name: 'Tools Skills',
+                        description: [
+                            {
+                                acomplishment: 'GitHub, Git, Microsoft Azure, Planview Leankit',
+                                dateStart: new Date('', '', ''),
+                                dateEnd: new Date('', '', '')
+                            }
+                        ]
+                    },
+                ]
+            },
+            {
+                title: 'Campus & Community Engagement',
+                place: [
+                    {
+                        name: 'Volunteer work',
+                        description: [
+                            {
+                                acomplishment: '80 hours for Q-Center at Binghamton University',
+                                dateStart: new Date('', '', ''),
+                                dateEnd: new Date('', '', '')
+                            },
+                            {
+                                acomplishment: '420 hours for Manhattan Valley Golden Age Center at the Red Oak',
+                                dateStart: new Date('', '', ''),
+                                dateEnd: new Date('', '', '')
+                            },
+                            {
+                                acomplishment: '50 hours for Rutgers Presbyterian Church soup kitchen',
+                                dateStart: new Date('', '', ''),
+                                dateEnd: new Date('', '', '')
+                            },
+                            {
+                                acomplishment: '200 hours for West Presbyterian Church in Binghamton soup kitchen',
+                                dateStart: new Date('', '', ''),
+                                dateEnd: new Date('', '', '')
+                            }
+                        ]
+                    },
+                    {
+                        name: 'Computer Programmers United',
+                        description: [
+                            {
+                                acomplishment: 'Competed in Consortium for Computing Sciences in Colleges Northeastern (CCSCNE) placing highest ever for our college',
+                                dateStart: new Date('', '', ''),
+                                dateEnd: new Date('', '', '')
+                            },
+                            {
+                                acomplishment: 'Led Computer Programmers United (CPU) in SUNY College at Oneonta as officer',
+                                dateStart: new Date('', '', ''),
+                                dateEnd: new Date('', '', '')
+                            }
+                        ]
+                    },
+                    {
+                        name: 'First Robotics Competition',
+                        description: [
+                            {
+                                acomplishment: 'Competed in and won the Rochester Regional going on to the US Nationals',
+                                dateStart: new Date('', '', ''),
+                                dateEnd: new Date('', '', '')
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+        $scope.projectInfo = [
+            {
+                name: 'Personal Web-page',
+                description: 'A personal web page detailing my skills, and hobbies',
+                details: 'Done using basic html for structure, css for styling, and Angular.JS for some static & dynamic elements to highlight skills on my resume, and some personal hobbies of mine',
+                date: new Date('', '', ''),
+                link:'https://github.com/hyperbolicj/Personal-Web-Page'
+            },
+            {
+                name: 'Blood testing Interpretation Library',
+                description: 'software module using data derived from imaged prepared blood samples to determine possible bacterial infections',
+                details: 'Data extraction was done using strategy pattern to handle various data file formats used by preceeding module. The driver of the interpratation library was implemented using singleton pattern to ensure memory overheads were not exceeded. The driver also implemented object pool pattern to have a fixed number of interpraters to handle processing of seperate blood samples asynchronously.',
+                date: new Date('2020', '00', '03'),
+                link: ''
+            },
+            {
+                name: 'Full Knee Arthroplasty Robot Documentation',
+                description: 'Documentation of code for several project modules for a robot designed to assist in Full Knee Arthroplasty surgery',
+                details: 'Made recommendations to testing teams, and developers regarding current test coverage shortfalls, and  inconsistencies in coding standards.',
+                date: new Date('2020', '00', '03'),
+                link: ''
+            },
+            {
+                name: 'MS Capstone project',
+                description: 'Java & C# application that attempts to create a mapping between a transcript and the accompanying audio file. To begin an audio playback from any user specified word',
+                details: 'The app uses dynamic gui view to switch between the 3 core components of the application. The sampler which samples audio files for use by both the neural network training component, & main user interface component, the neural network trainer used to train a new model given the necessary files, and the main user interface used to apply a model to create a mapping between a transcript & the audio file.',
+                date: new Date('2019', '03', '25'),
+                link: ''
+            }
         ]
 	}
 	]
